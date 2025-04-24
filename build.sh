5a900974ae986082a2796ae53644becbde2f78ef
Michael Wolf <susonicth@gmail.com>
2025-04-24 23:21:20 +0200

    added --test and --release; fixed argument error output

--- build.sh
+++ build.sh
@@ -6,17 +6,19 @@ function print_help() {
     echo "usage ./build.sh [OPTIONs]"
     echo ""
     echo "options:"
-    echo "        --help   print this help"
-    echo "        --all    build for all platforms (output in bin with platform suffix)"
-    echo "        --upx    compress binary with upx"
-    echo "        --clean  delete .zig-cache, zig-out and files in bin"
+    echo "        --help     print this help"
+    echo "        --all      build for all platforms (output in bin with platform suffix)"
+    echo "        --upx      compress binary with upx"
+    echo "        --clean    delete .zig-cache, zig-out and files in bin"
+    echo "        --test     run tests (before build)"
+    echo "        --release  prepare release (all flags set)"
     echo ""
 }
 
 function exit_on_error() {
     if [ $? -ne 0 ]; then
         >&2 echo ""
-        >&2 echo "an error occured, stopping."
+        >&2 echo "an error occured, stopping build"
         >&2 echo ""
         exit 1
     fi
@@ -34,6 +36,8 @@ ALL="false"
 UPX="false"
 TEST="false"
 CLEAN="false"
+TESTS="false"
+RELEASE="false"
 
 while (( "$#" )); do
     case "$1" in
@@ -41,7 +45,9 @@ while (( "$#" )); do
         --all )         ALL="true"; shift;;
         --upx )         UPX="true"; shift;;
         --clean )       CLEAN="true"; shift;;
-        * ) print_help; exit_argument_error "unknown command $1";;
+        --test )        TESTS="true"; shift;;
+        --release )     CLEAN="true"; ALL="true"; UPX="true"; TESTS="true"; RELEASE="true"; shift;;
+        * ) exit_argument_error "unknown command $1";;
     esac
 done
 
@@ -49,6 +55,11 @@ if [ "$CLEAN" == "true" ]; then
     echo cleaning
     rm -fr zig-out .zig-cache >/dev/null 2>&1
     rm bin/* >/dev/null 2>&1
+fi
+
+if [ "$TESTS" == "true" ]; then
+    echo testing
+    zig test src/main.zig || exit_on_error
 fi
 
 NATIVE_SUFFIX=""
@@ -74,18 +85,39 @@ function build_platform() {
     #compress with UPX if requested, else copy
     if [ "$UPX" = "true" ]; then
         echo compressing $PLAT
-        upx -qq --lzma -o $EXE_NAME $COMPILED_BIN
-    else
+        if [[ "$RELEASE" == "true" ]] && [[ "$PLAT" != "native" ]]; then        
+            upx -qq --lzma $COMPILED_BIN
+        else
+            upx -qq --lzma -o $EXE_NAME $COMPILED_BIN
+        fi
+    else 
         cp $COMPILED_BIN $EXE_NAME
+    fi
+
+    #create release packages
+    if [[ "$RELEASE" == "true" ]] && [[ "$PLAT" != "native" ]]; then
+        echo packaging $PLAT
+        if [ "$SUFFIX" == ".exe" ]; then 
+            cd zig-out/bin/
+            zip a ../../bin/csvdiff-${PLAT}.zip csvdiff.exe > /dev/null
+            cd ../../
+        else 
+            tar -czf ./bin/csvdiff-${PLAT}.tgz -C zig-out/bin/ csvdiff
+        fi
     fi
 }
 
 # build native
+echo
 build_platform "native" $NATIVE_SUFFIX
 
 # build all
 if [ "$ALL" = "true" ]; then
+    echo
     build_platform "x86_64-windows" ".exe"
+    echo
     build_platform "x86_64-linux"
+    echo
     build_platform "aarch64-linux"
+    echo
 fi
